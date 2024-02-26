2a16329b63eae548c5dce49d9f522eb6a9bfb6ca
amarov <boyko.amarov@gmail.com>
2024-02-26 17:00:58 +0200

    s

--- script.R
+++ script.R
@@ -4,6 +4,14 @@ library(patchwork)
 library(plotly)
 library(GGally)
 library(broom)
-library(caret)R
+library(caret)
+
+options(
+  repos = c(RSPM = "https://packagemanager.rstudio.com/all/latest",
+            CRAN = "https://cran.rstudio.com/"),
+  renv.config.auto.snapshot = TRUE,
+  renv.config.ppm.enabled = TRUE,
+  renv.config.ppm.default = TRUE
+)
  
  list(ppm.enabled = TRUE)
